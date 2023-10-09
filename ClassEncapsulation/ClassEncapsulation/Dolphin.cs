955816806a88d2efb9ef8667c4be3de25adae754
unknown <bd7q9887u@code.edu.az>
2023-10-10 00:55:34 +0400

    ClassEncapsulation

---
+++ ClassEncapsulation/ClassEncapsulation/Dolphin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEncapsulation
+{
+    internal class Dolphin : Animal
+    {
+
+        public Dolphin(string Name, int Age):base(Name, Age)
+        {
+        }
+    }
+}
